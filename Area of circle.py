5ff831317d72e3e6a90193108c7b501b9889eec7
Megha206 <85554743+Megha206@users.noreply.github.com>
2021-06-08 15:31:14 +0530

    Add files via upload

---
+++ Area of circle.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  8 15:08:01 2021
+
+@author: Megha Rao
+"""
+
+
+radius=float(input('Input the radius of the circle'))
+area=3.14*(radius**2)
+print(f'The area of he circle with radius {radius} is {area}')
